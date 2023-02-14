5265f1ddcb5a25d2723055bb4d3f971b6bf60633
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-02-14 19:42:53 -0300

    creating the home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,5 +1,67 @@
-import { LayoutContainer } from './styles'
+import {
+  LayoutContainer,
+  HomeSection,
+  TextContainer,
+  IconsTable,
+  TitleAndSubtitleContainer,
+  CoffeeSection,
+  CoffeesTable,
+} from './styles'
+import { ShoppingCart, Timer, Package, Coffee } from 'phosphor-react'
+import coffee from '../../assets/coffee.svg'
+import { InfoIcon } from '../../components/InfoIcon'
+import { CardCoffee } from './CardCoffee'
+import { coffees } from '../../mock/coffees'
 
 export function Home() {
-  return <LayoutContainer></LayoutContainer>
+  return (
+    <LayoutContainer>
+      <HomeSection>
+        <TextContainer>
+          <TitleAndSubtitleContainer>
+            <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+            <h3>
+              Com o Coffee Delivery você recebe seu café onde estiver, a
+              qualquer hora
+            </h3>
+          </TitleAndSubtitleContainer>
+
+          <IconsTable>
+            <InfoIcon
+              icon={<ShoppingCart size={16} color="white" weight="duotone" />}
+              text={'Compra simples e segura'}
+              backgroundColor={'orange'}
+            />
+            <InfoIcon
+              icon={<Timer size={16} color="white" weight="duotone" />}
+              text={'Entrega rápida e rastreada'}
+              backgroundColor={'yellow'}
+            />
+            <InfoIcon
+              icon={<Package size={16} color="white" weight="duotone" />}
+              text={'Embalagem mantém o café intacto'}
+              backgroundColor={'gray'}
+            />
+            <InfoIcon
+              icon={<Coffee size={16} color="white" weight="duotone" />}
+              text={'O café chega fresquinho até você'}
+              backgroundColor={'purple'}
+            />
+          </IconsTable>
+        </TextContainer>
+
+        <img src={coffee} alt="Ilustração do copo da Coffee Delivery" />
+      </HomeSection>
+
+      <CoffeeSection>
+        <h1>Nossos cafés</h1>
+
+        <CoffeesTable>
+          {coffees.map((coffee) => (
+            <CardCoffee key={coffee.id} coffee={coffee} />
+          ))}
+        </CoffeesTable>
+      </CoffeeSection>
+    </LayoutContainer>
+  )
 }
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,105 @@
 import styled from 'styled-components'
 
-export const LayoutContainer = styled.div``
+export const LayoutContainer = styled.main`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`
+
+export const HomeSection = styled.section`
+  width: 100%;
+  margin-top: 2rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: 30px;
+
+  & img {
+    width: 29.75rem;
+    //background-color: yellow;
+  }
+
+  @media (max-width: 768px) {
+    display: block;
+
+    & img {
+      width: 20rem;
+    }
+  }
+  @media (max-width: 820px) {
+    & img {
+      width: 20rem;
+    }
+  }
+  @media (min-width: 1215px) {
+    & img {
+      width: 47rem;
+    }
+  }
+`
+
+export const TitleAndSubtitleContainer = styled.div`
+  margin-bottom: 4.125rem;
+`
+
+export const TextContainer = styled.div`
+  //width: 36.75rem;
+  //background-color: blue;
+
+  h1 {
+    font-family: 'Baloo 2', sans-serif;
+    font-weight: 700;
+    font-size: 2.5rem;
+    color: ${(props) => props.theme['gray-900']};
+    line-height: 130%;
+  }
+
+  h3 {
+    font-weight: 400;
+    color: ${(props) => props.theme['gray-800']};
+    font-size: 1.25rem;
+  }
+
+  @media (min-width: 1355px) {
+    width: 50%;
+
+    h1 {
+      font-size: 3.5rem;
+    }
+  }
+`
+
+export const IconsTable = styled.div`
+  display: grid;
+  column-gap: 10px;
+  grid-template-columns: 50% 50%;
+  row-gap: 1.25rem;
+`
+
+export const CoffeeSection = styled.div`
+  h1 {
+    font-family: 'Baloo 2', sans-serif;
+    font-weight: 700;
+    font-size: 2rem;
+    color: ${(props) => props.theme['gray-800']};
+    line-height: 130%;
+  }
+`
+
+export const CoffeesTable = styled.div`
+  display: grid;
+  column-gap: 10px;
+  grid-template-columns: auto auto auto auto;
+
+  @media (max-width: 768px) {
+    display: grid;
+    column-gap: 10px;
+    grid-template-columns: auto;
+  }
+  @media (max-width: 820px) {
+    display: grid;
+    column-gap: 10px;
+    grid-template-columns: auto auto;
+  }
+`
