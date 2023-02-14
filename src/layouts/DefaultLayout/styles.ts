b855bb0f90f26e5f60db0fd63678b7b1b91517a2
leticiar-io <leticia.rodrigues20019@gmail.com>
2023-02-14 19:43:27 -0300

    creating a default layout

--- src/layouts/DefaultLayout/styles.ts
+++ src/layouts/DefaultLayout/styles.ts
@@ -1,5 +1,12 @@
 import styled from 'styled-components'
 
 export const LayoutContainer = styled.div`
-  padding: 2rem 16rem 0 16rem; /* top | right | bottom | left */
+  padding: 2rem 16rem 20rem 16rem; /* top | right | bottom | left */
+
+  @media (max-width: 768px) {
+    padding: 1rem;
+  }
+  @media (max-width: 1324px) {
+    padding: 1rem 3.5rem 10rem 3.5rem;
+  }
 `
